24be29572f75f47a2b6934cb54a17b6b1ffa4e0e
Ramen <Ramen@DESKTOP-MI2VOE6>
2019-12-29 03:21:04 +0100

    Inserito annunci già cercati, storico annunci già passati

---
+++ GratisForGratis/Models/Enumerators/PosizioneVisibilitaEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GratisForGratis.Models.Enumerators
+{
+    public enum PosizioneVisibilitaEnum
+    {
+        Nessuna = 0,
+        HomeProfilo = 1
+    }
+}
